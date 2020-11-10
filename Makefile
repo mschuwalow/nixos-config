a30a0d0547c7db58b2cf73056d698676e6f367cf
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2020-11-10 14:10:02 +0100

    add home-manager as checkout

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ PROJECT_DIR := $(dir $(realpath $(firstword $(MAKEFILE_LIST))))
 .PHONY: fmt
 
 fmt:
-	nixfmt $$(find ${PROJECT_DIR} -name '*.nix' -not -path "${PROJECT_DIR}nixpkgs/*")
+	nixfmt $$(find ${PROJECT_DIR} -name '*.nix' ! -path "${PROJECT_DIR}nixpkgs/*" ! -path "${PROJECT_DIR}modules/home-manager/*")
--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,16 +1,8 @@
-{ config, pkgs, lib, ... }:
-let
-  rev = "4cc1b77c3fc4f4b3bc61921dda72663eea962fa3";
-  sha256 = "02y6bjakcbfc0wvf9b5qky792y9abyf1kgnk8r30f1advn3x69nc";
-  home-manager = fetchTarball {
-    inherit sha256;
-    url = "https://github.com/rycee/home-manager/archive/${rev}.tar.gz";
-  };
-in {
+{ config, pkgs, lib, ... }: {
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
   };
-  imports = [ "${home-manager}/nixos" ];
-  vars.hmLib = import "${home-manager}/modules/lib" { inherit lib; };
+  imports = [ ./home-manager/nixos ];
+  vars.hmLib = import ./home-manager/modules/lib { inherit lib; };
 }
