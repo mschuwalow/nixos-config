131a892297ba0703f52a59a80e0f5a559512f50a
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2020-12-01 03:05:19 +0100

    update checkouts

--- configuration.nix
+++ configuration.nix
@@ -81,7 +81,10 @@ in {
     pciutils
   ];
 
-  i18n = { defaultLocale = "en_US.UTF-8"; };
+  i18n = { 
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = ["en_US.UTF-8/UTF-8"];
+  };
 
   nix = {
     nixPath = [
