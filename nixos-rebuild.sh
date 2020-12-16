3a5e99ef87aacbf08c1673aeb62ee740cba2c72b
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2020-12-16 23:00:13 +0100

    update nixos-rebuild script

--- nixos-rebuild.sh
+++ nixos-rebuild.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-exec nixos-rebuild -I nixpkgs=./checkouts/nixpkgs -I nixpkgs-overlays -I nixos-config=./configuration.nix "$@"
+export NIX_PATH="nixpkgs=./checkouts/nixpkgs:nixos-config=./configuration.nix"
+exec nixos-rebuild "$@"
