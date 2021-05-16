0da030d0704a23ebdec9f376c86b19420056f100
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2021-05-16 18:09:48 +0200

    update locale & usernames

--- configuration.nix
+++ configuration.nix
@@ -72,10 +72,7 @@
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    supportedLocales = [ 
-      "en_US.UTF-8/UTF-8"
-      "de_DE.UTF-8/UTF-8"
-    ];
+    supportedLocales = [ "en_US.UTF-8/UTF-8" "de_DE.UTF-8/UTF-8" ];
   };
 
   nix = {
--- overlays/unite-custom.nix
+++ overlays/unite-custom.nix
@@ -1,13 +1,12 @@
 self: super: {
   gnomeExtensions = super.gnomeExtensions // {
-    unite =
-      super.gnomeExtensions.unite.overrideAttrs (old: rec {
-        src = super.fetchFromGitHub {
-          owner = "mschuwalow";
-          repo = "unite-shell";
-          rev = "b424726e6beed1a047dc19d5202292ac41861f21";
-          sha256 = "fWW2O3LXb8eNJU/hOB2TfxyZ4C/yQa2NVoe5eotOOYw=";
-        };
-      });
+    unite = super.gnomeExtensions.unite.overrideAttrs (old: rec {
+      src = super.fetchFromGitHub {
+        owner = "mschuwalow";
+        repo = "unite-shell";
+        rev = "b424726e6beed1a047dc19d5202292ac41861f21";
+        sha256 = "fWW2O3LXb8eNJU/hOB2TfxyZ4C/yQa2NVoe5eotOOYw=";
+      };
+    });
   };
 }
