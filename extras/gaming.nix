53a6963b09456f585d54b23394eed6e41c7a722e
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2021-12-13 19:26:32 +0100

    switch back to gnome

--- extras/gaming.nix
+++ extras/gaming.nix
@@ -12,21 +12,21 @@
 
 
   # diablo 2 dclone filters
-  networking.firewall.extraCommands = ''
-    iptables -N LOG_DROP || true
-    iptables -F LOG_DROP
-    iptables -A LOG_DROP -j LOG --log-prefix "IPTables-Dropped: " --log-level 6
-    iptables -A LOG_DROP -j DROP
-
-    iptables -N d2r-dclone || true
-    iptables -F d2r-dclone
-    iptables -A d2r-dclone -d 34.93.251.115 -j ACCEPT
-    iptables -A d2r-dclone -m iprange --dst-range 158.115.1.1-158.115.206.255 -j LOG_DROP
-    iptables -A d2r-dclone -m iprange --dst-range 158.115.211.1-158.115.255.255 -j LOG_DROP
-    iptables -A d2r-dclone -m iprange --dst-range 34.1.1.1-34.116.255.255 -j LOG_DROP
-    iptables -A d2r-dclone -m iprange --dst-range 34.118.1.1-34.255.255.255 -j LOG_DROP
-    iptables -A d2r-dclone -m iprange --dst-range 35.1.1.1-35.255.255.255 -j LOG_DROP
-    iptables -A d2r-dclone -j RETURN
-    # iptables -I OUTPUT -j d2r-dclone
-  '';
+  #networking.firewall.extraCommands = ''
+  #  iptables -N LOG_DROP || true
+  #  iptables -F LOG_DROP
+  #  iptables -A LOG_DROP -j LOG --log-prefix "IPTables-Dropped: " --log-level 6
+  #  iptables -A LOG_DROP -j DROP
+  #
+  #  iptables -N d2r-dclone || true
+  #  iptables -F d2r-dclone
+  #  iptables -A d2r-dclone -d 34.93.251.115 -j ACCEPT
+  #  iptables -A d2r-dclone -m iprange --dst-range 158.115.1.1-158.115.206.255 -j LOG_DROP
+  #  iptables -A d2r-dclone -m iprange --dst-range 158.115.211.1-158.115.255.255 -j LOG_DROP
+  #  iptables -A d2r-dclone -m iprange --dst-range 34.1.1.1-34.116.255.255 -j LOG_DROP
+  #  iptables -A d2r-dclone -m iprange --dst-range 34.118.1.1-34.255.255.255 -j LOG_DROP
+  #  iptables -A d2r-dclone -m iprange --dst-range 35.1.1.1-35.255.255.255 -j LOG_DROP
+  #  iptables -A d2r-dclone -j RETURN
+  #  # iptables -I OUTPUT -j d2r-dclone
+  #'';
 }
--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -26,11 +26,24 @@
       vlc
       xclip
       xdotool
+      yaru-theme
       zoom-us
     ]) ++ (with pkgs.gnome; [
       dconf-editor
       gnome-disk-utility
       gnome-system-monitor
+      gnome-tweak-tool
+    ]) ++ (with pkgs.unstable.gnomeExtensions; [
+      caffeine
+      clipboard-indicator
+      dash-to-dock
+      dash-to-panel
+      gsconnect
+      gtile
+      gtk-title-bar
+      material-shell
+      tiling-assistant
+      unite
     ]);
   };
 
@@ -42,6 +55,14 @@
       (nerdfonts.override { fonts = [ "Hack" ]; })
       iosevka-custom
     ];
+    fontconfig = {
+      defaultFonts = {
+        emoji = [ "Twitter Color Emoji" ];
+        monospace = [ "Ubuntu Mono" ];
+        sansSerif = [ "Ubuntu" ];
+        serif = [ "Ubuntu Condensed" ];
+      };
+    };
   };
 
   hardware.opengl = {
@@ -59,6 +80,12 @@
 
   networking.firewall = {
     allowedTCPPorts = [ 57621 ]; # spotify
+    allowedTCPPortRanges = [
+      { from = 1714; to = 1764; } # kde-connect
+    ];
+    allowedUDPPortRanges = [
+      { from = 1714; to = 1764; } # kde-connect
+    ];
   };
 
   programs.pantheon-tweaks.enable = true;
@@ -67,9 +94,35 @@
     flatpak.enable = true;
     xserver = {
       desktopManager = {
-        pantheon.enable = true;
+        gnome = {
+          enable = true;
+          extraGSettingsOverrides = ''
+            [org.gnome.desktop.interface]
+            gtk-theme='Yaru-dark'
+            icon-theme='Yaru'
+            cursor-theme='Yaru'
+            font-name='Ubuntu 11'                                                                                                                 
+            document-font-name='Ubuntu Condensed 11'                                                                                                          
+            monospace-font-name='Ubuntu Mono 10'                                                                                                  
+            
+            [org.gnome.desktop.sound]
+            theme-name='Yaru'
+            [org/gnome/desktop/wm/preferences]
+            titlebar-font='Ubuntu Bold 11'
+            [desktop.ibus.panel.emoji]
+            hotkey=[]
+            [org/gnome/desktop/input-sources]
+            xkb-options=['terminate:ctrl_alt_bksp', 'lv3:ralt_switch', 'compose:caps']
+            [org/gnome/gedit/preferences/editor]
+            scheme='Yaru-dark'
+          '';
+          extraGSettingsOverridePackages = with pkgs.gnome; [
+            gedit
+          ];
+        };
         xterm.enable = false;
       };
+      displayManager.gdm.enable = true;
       enable = true;
       layout = "us";
     };
